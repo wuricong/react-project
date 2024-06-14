e1531bf1a5c26fa545d9cf76024f8266fbe5a71d
wuricong-es-prod <15070301734@163.com>
2024-06-14 17:49:50 +0800

    feat: 更改vite配置，引用资源地址变成相对路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
-  server:{
-    host:'0.0.0.0'
+  server: {
+    host: '0.0.0.0'
   },
-  css:{
-    preprocessorOptions:{
-      math:'always'
+  css: {
+    preprocessorOptions: {
+      math: 'always'
     }
   }
 })
