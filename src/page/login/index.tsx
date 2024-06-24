0b01bbfcb3eaf7cb95223eccb02c8c2c6fb30323
wuricong-es-prod <15070301734@163.com>
2024-06-24 10:39:24 +0800

    fix: 修复css背景图无法显示问题

--- src/page/login/index.tsx
+++ src/page/login/index.tsx
@@ -1,8 +1,13 @@
 import "./index.css";
+import Background from "../../assets/bg.jpg";
+
+const styleUrl = {
+  backgroundImage: `url(${Background})`,
+};
 
 function Login() {
   return (
-    <div className="box">
+    <div className="box" style={styleUrl}>
       <div className="login-form"></div>
     </div>
   );
