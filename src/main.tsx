aa5cd647e5c1e51f7ad9517b4b24e7e0cdfd237f
wuricong <2260220325@163.com>
2024-08-24 11:36:07 +0800

    feat:测试部署

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,10 @@ import "./styles/font.css"; //引入字体文件
 import "./index.css";
 import "./assets/iconfont/iconfont.css";
 
-console.log(111, import.meta.env.MODE);
+console.log(111222, import.meta.env.MODE);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter
-    basename={import.meta.env.MODE === "development" ? "" : " /react-project/"}
+    basename={import.meta.env.MODE === "development" ? "" : " /react-project"}
   >
     <React.StrictMode>
       <ConfigProvider
