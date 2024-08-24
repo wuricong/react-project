3b607fd5f1fc7261574800967f07f479a85fdf10
wuricong <2260220325@163.com>
2024-08-24 11:16:25 +0800

    feat: 根据环境区分页面路径

--- src/main.tsx
+++ src/main.tsx
@@ -9,9 +9,10 @@ import "./styles/font.css"; //引入字体文件
 import "./index.css";
 import "./assets/iconfont/iconfont.css";
 
+console.log(111, import.meta.env.MODE);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter
-    basename={process.env.NODE_ENV === "development" ? "" : " /react-project/"}
+    basename={import.meta.env.MODE === "development" ? "" : " /react-project/"}
   >
     <React.StrictMode>
       <ConfigProvider
