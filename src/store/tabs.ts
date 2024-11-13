81e45766d38b229c1e7c69f07eb0abda3bc81b8b
wuricong <2260220325@163.com>
2024-11-13 18:04:24 +0800

    feat:配置项目启动时自动打开浏览器

--- src/store/tabs.ts
+++ src/store/tabs.ts
@@ -1,26 +1,17 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// console.log(
-//   'sessionStorage.getItem("activeKey")',
-//   sessionStorage.getItem("activeKey"),
-// );
 const useTabsStore = createSlice({
   name: "tabs",
   initialState: {
-    activeKey: "1",
-    menuActive: "1",
+    activeKey: sessionStorage.getItem("activeKey") || "1",
   },
   reducers: {
     changeActive(state, action) {
-      // localStorage.setItem("activeKey", action.payload);
       state.activeKey = action.payload;
-    },
-    changeMenuActive(state, action) {
-      state.menuActive = action.payload;
     },
   },
 });
 
-export const { changeActive, changeMenuActive } = useTabsStore.actions;
+export const { changeActive } = useTabsStore.actions;
 
 export default useTabsStore.reducer;
--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
   },
   plugins: [react()],
   server: {
+    open: true,
     host: "0.0.0.0",
   },
   css: {
