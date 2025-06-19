c8bdc4dc602a7f94bfe5fa4e9f37999f27d6c8cb
wuricong <2260220325@163.com>
2025-06-19 17:05:48 +0800

    feat: 测试打包选项

--- src/page/404.tsx
+++ src/page/404.tsx
@@ -1,14 +1,20 @@
 import imgSrc from "@/assets/404.jpg";
+import { useNavigate } from "react-router-dom";
 
 function NotFound() {
+  const handleBack = () => {
+    const navigate = useNavigate();
+    navigate("/login");
+  };
   return (
     <>
       <div
         className="w-full h-full flex items-center justify-center"
         style={{ background: "#CBCBD3" }}
       >
-        <img src={imgSrc} />
+        <img src={imgSrc} alt="" />
       </div>
+      <button onClick={handleBack}>返回</button>
     </>
   );
 }
