2bde4fd506437606c860793ce1ebd09a936462c9
wuricong <2260220325@163.com>
2025-06-19 16:29:01 +0800

    feat: 测试打包选项

--- src/page/404.tsx
+++ src/page/404.tsx
@@ -3,7 +3,10 @@ import imgSrc from "@/assets/404.jpg";
 function NotFound() {
   return (
     <>
-      <div className="w-full h-full">
+      <div
+        className="w-full h-full flex items-center justify-center"
+        style={{ background: "#CBCBD3" }}
+      >
         <img src={imgSrc} />
       </div>
     </>
