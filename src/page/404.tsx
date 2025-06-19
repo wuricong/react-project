dd5f0a95a0c0f273e8540982b099ecd361483483
wuricong <2260220325@163.com>
2025-06-19 17:11:28 +0800

    feat: 测试打包选项

--- src/page/404.tsx
+++ src/page/404.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 function NotFound() {
   const handleBack = () => {
     const navigate = useNavigate();
-    navigate("/login");
+    navigate("login");
   };
   return (
     <>
